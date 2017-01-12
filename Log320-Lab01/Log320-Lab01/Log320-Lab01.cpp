f2742e14d06a28c1936b1fe683a54ca4467d5d67
Manda <mandaniaina@gmail.com>
2017-01-12 10:35:40 -0500

    calculatrice

--- Log320-Lab01/Log320-Lab01/Log320-Lab01.cpp
+++ Log320-Lab01/Log320-Lab01/Log320-Lab01.cpp
@@ -8,7 +8,26 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
+	double a(0), b(0); //Déclaration des variables utiles
+
+	cout << "Bienvenue dans le programme de calcul de a^b !" << endl;
+
+	cout << "Donnez une valeur pour a : "; //On demande le premier nombre
+	cin >> a;
+
+	cout << "Donnez une valeur pour b : "; //On demande le deuxième nombre
+	cin >> b;
+
+	double const resultat(pow(a, b)); //On effectue l'opération
+									  //On peut aussi écrire comme avant :
+									  //double const resultat = pow(a,b);
+									  //Souvenez-vous des deux formes possibles
+									  //De l'initialisation d'une variable
+
+	cout << a << " ^ " << b << " = " << resultat << endl;
+	//On affiche le résultat
+	cin >> b;
 	return 0;
+
 }
 
