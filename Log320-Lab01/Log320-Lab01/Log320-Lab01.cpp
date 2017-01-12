b9ce338331f337d76f4764fe0ffb650673f2c3d1
Manda <mandaniaina@gmail.com>
2017-01-12 11:21:35 -0500

    lecture

--- Log320-Lab01/Log320-Lab01/Log320-Lab01.cpp
+++ Log320-Lab01/Log320-Lab01/Log320-Lab01.cpp
@@ -3,29 +3,22 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	double a(0), b(0); //Déclaration des variables utiles
-
-	cout << "Bienvenue dans le programme de calcul de a^b !" << endl;
-
-	cout << "Donnez une valeur pour a : "; //On demande le premier nombre
-	cin >> a;
-
-	cout << "Donnez une valeur pour b : "; //On demande le deuxième nombre
-	cin >> b;
-
-	double const resultat(pow(a, b)); //On effectue l'opération
-									  //On peut aussi écrire comme avant :
-									  //double const resultat = pow(a,b);
-									  //Souvenez-vous des deux formes possibles
-									  //De l'initialisation d'une variable
-
-	cout << a << " ^ " << b << " = " << resultat << endl;
-	//On affiche le résultat
+	double b(15);
+	std::ifstream file("dict.txt");
+	std::string str;
+	int nbr(0);
+	while (std::getline(file, str))
+	{
+		nbr++;
+		cout << "Mot numéro: " << nbr << " " << str << endl;
+	}
 	cin >> b;
 	return 0;
 
